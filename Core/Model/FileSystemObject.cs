52a854b1723cdccc27b10f60fce52920ac187e80
KaShTaN27 <dkachan.lida@mail.ru>
2022-11-23 21:48:11 +0300

    Add file system object model

---
+++ Core/Model/FileSystemObject.cs
@@ -0,0 +1,43 @@
+﻿namespace Core.Model;
+
+public class FileSystemObject
+{
+    public string Path { get; }
+    public string Name { get; }
+    public float Percentage;
+    public long Size;
+    public IList<FileSystemObject> Childs = new List<FileSystemObject>();
+
+    public FileSystemObject(string path, string name, long size)
+    {
+        Size = size;
+        Path = path;
+        Name = name;
+    }
+
+    public FileSystemObject(string path, string name)
+    {
+        Path = path;
+        Name = name;
+    }
+
+    public void ComputeStatistics() {
+        ComputeSize();
+        ComputePercentage();
+    }
+
+    private long ComputeSize() {
+        foreach (var child in Childs) {
+            child.ComputeSize();
+            Size += child.Size;
+        }
+        return Size;
+    }
+
+    private void ComputePercentage() {
+        foreach (var child in Childs) {
+            child.Percentage = (float)child.Size / this.Size * 100;
+            child.ComputePercentage();
+        }
+    }
+}
